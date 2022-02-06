30b99c84152af3a2611badcf17a5552616e090a7
xysiek99 <xysiek99@gmail.com>
2022-02-06 01:50:04 +0100

    Making usage of classes

--- desktopConsoleApp.py
+++ desktopConsoleApp.py
@@ -2,46 +2,74 @@ from main import decimalToBinary, binaryToDecimal
 import os
 
 options = ["Binary to decimal", "Decimal to binary", "Exit"]
-menu_range = range(1,4)
-binary_range = range(0,2)
+options_range = range(1, len(options) + 1)
+binary_values = range(0,2)
+decimal_values = range(0,10)
 
-def headerDisplay():
-    os.system('cls||clear')
-    print("BINARY & DECIMAL CALCULATOR")
-    print("---------------------------")
-
-def menuDisplay():
-    i = 1
-    for opt in options:
-        print(f"{i}. {opt}")
-        i += 1
-
-def checkInt(number, numberList):
+# def menuLoop():
+#     incorrectInput = True
+#     while incorrectInput:
+#         user_input = input()
+#         if (checkInt(user_input, options_range)):
+#             incorrectInput = False
+    
+#     if (int(user_input) == 1):
+#         print(options[0])
+#     if (int(user_input) == 2):
+#         print(options[1])
+#     if (int(user_input) == 3):
+#         print(options[2])
+    
+def checkOption(input_number, input_range):
     try:
-        if int(number) in numberList:
+        if int(input_number) in input_range:
             return True
         else:
             raise Exception
     except:
         print("Incorrect input")
 
+def checkValue():
+    pass
 
-def menuLoop():
-    incorrectInput = True
-    while incorrectInput:
-        userInput = input()
-        if (checkInt(userInput, menu_range)):
-            incorrectInput = False
-    
-    if (int(userInput) == 1):
-        print(options[0])
-    if (int(userInput) == 2):
-        print(options[1])
-    if (int(userInput) == 3):
-        print(options[2])
-    
+class Menu:
+    def __init__(self, header, options = None, input_range = None, accepted_values = None):
+        self.header = header
+        self.options = options
+        self.input_range = input_range
+        self.accepted_values = accepted_values
+        self.incorrect_input = False
+
+    def displayHeader(self):
+        os.system('cls||clear')
+        print(self.header)
+        print("---------------------------")
+
+    def displayOptions(self):
+        i = 1
+        for option in self.options:
+            print(f"{i}. {option}")
+            i += 1
+
+    def loopMenu(self):
+        self.incorrect_input = True
+        while self.incorrect_input:
+            user_input = input()
+            if (checkOption(user_input, self.input_range)):
+                self.incorrectInput = False
+                break
+
+class BinaryMenu(Menu):
+    def __init__(self, header, options = None, input_range = None, accepted_values = None):
+        super().__init__(header, options, input_range, accepted_values)
+
+class DecimalMenu(Menu):
+    def __init__(self, header, options = None, input_range = None, accepted_values = None):
+        super().__init__(header, options, input_range, accepted_values)
 
 
-headerDisplay()
-menuDisplay()
-menuLoop()
+mainMenu = Menu("BINARY & DECIMAL CALCULATOR", options = options, input_range = options_range)
+binaryMenu = BinaryMenu("Enter binary value", accepted_values = binary_values)
+decimalMenu = DecimalMenu("Enter decimal value", accepted_values = decimal_values)
+
+mainMenu.loopMenu()
