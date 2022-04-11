4fe21c487312cf227001cfc5db2dec7fa7bb8abd
xysiek99 <xysiek99@gmail.com>
2022-04-11 23:07:53 +0200

    Added function to check if input is binary

--- desktopConsoleApp.py
+++ desktopConsoleApp.py
@@ -15,8 +15,17 @@ def checkOption(input_number, input_range):
     except:
         print("Incorrect input")
 
-def checkValue():
-    pass
+def checkIfDecimal(user_inp):
+    try:
+        if int(user_inp):
+            for i in user_inp:
+                if not (i=="0" or i=="1"):
+                    raise Exception
+            print(binaryToDecimal(int(user_inp)))
+        else:
+            raise Exception
+    except:
+        print("Incorrect input")
 
 class Menu:
     def __init__(self, header, options = None, input_range = None, accepted_values = None):
@@ -62,6 +71,8 @@ decimalMenu = DecimalMenu("Enter decimal value", accepted_values = decimal_value
 
 def startBinaryMenu():
     binaryMenu.displayHeader()
+    user_inp = input()
+    checkIfDecimal(user_inp = user_inp)
 
 def startDecimalMenu():
     decimalMenu.displayHeader()
@@ -73,7 +84,7 @@ def startMainMenu():
     while True:
         my_inp = input()
         if my_inp == "1":
-            binaryMenu.displayHeader()
+            startBinaryMenu()
         elif my_inp == "2":
             decimalMenu.displayHeader()
         elif my_inp == "3":
