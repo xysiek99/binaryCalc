493041ae661f3bdeeba5726b90cf7422b90a4354
Krzysiek <xysiek99@gmail.com>
2022-05-25 01:55:12 +0200

    Removed unused methods and properties

--- desktopConsoleApp.py
+++ desktopConsoleApp.py
@@ -5,15 +5,6 @@ options = ["Binary to decimal", "Decimal to binary", "Exit"]
 options_range = range(1, len(options) + 1)
 binary_values = range(0,2)
 decimal_values = range(0,10)
-    
-def checkOption(input_number, input_range):
-    try:
-        if int(input_number) in input_range:
-            return True
-        else:
-            raise Exception
-    except:
-        print("Incorrect input")
 
 def checkIfDecimal(user_inp):
     try:
@@ -37,11 +28,9 @@ def checkIfInt(user_inp):
         print("Incorrect input")   
 
 class Menu:
-    def __init__(self, header, options = None, input_range = None, accepted_values = None):
+    def __init__(self, header, options = None):
         self.header = header
         self.options = options
-        self.input_range = input_range
-        self.accepted_values = accepted_values
 
     def displayHeader(self):
         os.system('cls||clear')
@@ -54,39 +43,32 @@ class Menu:
             print(f"{i}. {option}")
             i += 1
 
-    def loopMenu(self):
-        while True:
-            user_input = input()
-            if (checkOption(user_input, self.input_range)):
-                break
-                #return user_input
-
 class BinaryMenu(Menu):
-    def __init__(self, header, options = None, input_range = None, accepted_values = None):
-        super().__init__(header, options, input_range, accepted_values)
+    def __init__(self, header, options = None):
+        super().__init__(header, options)
 
 class DecimalMenu(Menu):
-    def __init__(self, header, options = None, input_range = None, accepted_values = None):
-        super().__init__(header, options, input_range, accepted_values)
+    def __init__(self, header, options = None):
+        super().__init__(header, options)
 
 
-mainMenu = Menu("BINARY & DECIMAL CALCULATOR", options = options, input_range = options_range)
-binaryMenu = BinaryMenu("Enter binary value", accepted_values = binary_values)
-decimalMenu = DecimalMenu("Enter decimal value", accepted_values = decimal_values)
+mainMenu = Menu("BINARY & DECIMAL CALCULATOR", options = options)
+binaryMenu = BinaryMenu("Enter binary value")
+decimalMenu = DecimalMenu("Enter decimal value")
 
 
 def startBinaryMenu():
     binaryMenu.displayHeader()
     user_inp = input()
     checkIfDecimal(user_inp = user_inp)
-    input("Press any key...")
+    input("Press Enter...")
     startMainMenu()
 
 def startDecimalMenu():
     decimalMenu.displayHeader()
     user_inp = input()
     checkIfInt(user_inp = user_inp)
-    input("Press any key...")
+    input("Press Enter...")
     startMainMenu()
 
 def startMainMenu():
